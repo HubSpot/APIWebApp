9f78342da3aeff1d336b29e436af24cbd55b9f2c
glanza <gabrielaalanza@gmail.com>
2019-05-24 11:21:27 -0400

    Added lots of comments to the code

--- project/js/main.js
+++ project/js/main.js
@@ -1,7 +1,17 @@
+// This function commented out after adding the capability for users to search.
+// What would you do if you wanted the page to show the weather for a default city once loaded?
+
 // $(document).ready(function() {
 //   getWeather();
 // });
 
+// This function first resets the text fields to have no content.
+// Then, it creates the URL to send to OpenWeatherMap, including the content from the page's 
+//    search field.
+// Then, it performs an AJAX call to that URL.
+// If it's successful, the function takes the JSON data that was returned, and pulls the name 
+//    and temperature out of the object, then uses jQuery to display them on the page.
+// If there is an error, it shows an error message instead.
 function getWeather(searchQuery) {
   $(".error-message").text("");
   $(".city").text("");
@@ -17,6 +27,8 @@ function getWeather(searchQuery) {
   });
 }
 
+// This function grabs the value from the search input, then passes it along to the 
+//     getWeather() function.
 function searchWeather() {
   var searchQuery = $('.search').val(); // grab value from search input
   getWeather(searchQuery);
